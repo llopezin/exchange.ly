40a4c8ddaf414f222b4e3f5bb543dc672ca8bf5f
Laura Lopez <lauralopezinfantes@gmail.com>
2021-01-30 20:29:45 +0000

    state subscriptions added to historic rates component

--- src/app/views/components/historic-rates/historic-rates.component.ts
+++ src/app/views/components/historic-rates/historic-rates.component.ts
@@ -3,6 +3,8 @@ import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducers';
 import Rates from 'src/app/shared/models/rates.model';
 import { getHistoricRates } from 'src/app/shared/store/historic-rates-store/actions';
+import { overlayVisible } from 'src/app/shared/store/overlay-store/actions';
+import { getLatestRates } from 'src/app/shared/store/rates-store/actions';
 
 @Component({
   selector: 'app-historic-rates',
@@ -10,27 +12,26 @@ import { getHistoricRates } from 'src/app/shared/store/historic-rates-store/acti
   styleUrls: ['./historic-rates.component.scss'],
 })
 export class HistoricRatesComponent implements OnInit {
-  public loading: Boolean;
+  public loading: Boolean = true;
+  public overlayVisible: Boolean;
   public rates: Rates;
+  public latestRates: {};
   public startDate: string = '2018-01-01';
   public endDate: string = '2019-01-01';
   public dates = { start: this.startDate, end: this.endDate };
   public base: string = 'GBP';
-  public symbols: string[] = ['EUR'];
+  public selectedCurrencies: string[];
 
   constructor(private store: Store<AppState>) {}
-
   ngOnInit(): void {
-    this.subscribeToRatesStore();
-    this.subscribeToHistoricRatesStore();
+    this.initStorageSubscriptions();
+  }
 
-    this.store.dispatch(
-      getHistoricRates({
-        base: this.base,
-        symbols: this.symbols,
-        dates: this.dates,
-      })
-    );
+  initStorageSubscriptions() {
+    this.subscribeToRatesStore();
+    this.subscribeToUserCurrencyStore();
+    this.subscribeToHistoricRatesStore();
+    this.subscribeToOverlayStore();
   }
 
   subscribeToHistoricRatesStore() {
@@ -42,7 +43,44 @@ export class HistoricRatesComponent implements OnInit {
 
   subscribeToRatesStore() {
     this.store.select('ratesApp').subscribe((ratesResponse) => {
-      console.log(ratesResponse.rates);
+      this.latestRates = ratesResponse.rates;
     });
+  }
+
+  subscribeToOverlayStore() {
+    this.store.select('overlayApp').subscribe((overlayResponse) => {
+      this.overlayVisible = overlayResponse.visible;
+    });
+  }
+
+  subscribeToUserCurrencyStore() {
+    this.store.select('userCurrencyApp').subscribe(({ userCurrency, base }) => {
+      this.selectedCurrencies = userCurrency;
+      this.base = base;
+      if (this.ratesIsEmpty()) this.getLatestRates();
+      this.getHistoricRates();
+    });
+  }
+
+  getHistoricRates() {
+    this.store.dispatch(
+      getHistoricRates({
+        base: this.base,
+        symbols: this.selectedCurrencies,
+        dates: this.dates,
+      })
+    );
+  }
+
+  getLatestRates() {
+    this.store.dispatch(getLatestRates({ base: this.base }));
+  }
+
+  showOverlay() {
+    this.store.dispatch(overlayVisible({ visible: true }));
+  }
+
+  ratesIsEmpty() {
+    return Object.keys(this.latestRates).length === 0;
   }
 }
