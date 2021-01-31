c604aa10734742ac60db1e88107adbea23fccf6a
Laura Lopez <lauralopezinfantes@gmail.com>
2021-01-31 12:48:32 +0000

    header styles applied

--- src/app/views/components/layout/header/header.component.ts
+++ src/app/views/components/layout/header/header.component.ts
@@ -4,10 +4,16 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass'],
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  public menuVisible: boolean = false;
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  toggleMenu() {
+    this.menuVisible = !this.menuVisible;
+  }
 }
